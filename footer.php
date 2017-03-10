49f85d075cc185c18a431d7e81b74ebab2fdaf8f
ZhenweiZhang1995 <apple@MacBook-Pro-8.local>
2017-03-09 23:35:47 -0500

    Now using bulma css library

--- footer.php
+++ footer.php
@@ -2,14 +2,51 @@
       </div>
 
     </div>
+  </div>
 
-    <footer class="footer">
+    <!-- <footer class="footer">
       <div class="container">
-        <span class="text-muted">This is the place for credit</span>
+        <div class="content has-text-centered">
+          <p>
+            <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+      </p>
+        <p>
+          <a class="icon" href="https://github.com/jgthms/bulma">
+            <i class="fa fa-github"></i>
+        </a>
+        </p>
       </div>
-    </footer>
+    </div>
+  </footer> -->
 
+  <section class="hero is-info is-small">
+  <div class="hero-body">
+    <div class="container">
+
+      <div class="content has-text-centered">
+        <p>
+          <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+      </p>
+      <p>
+        <a class="icon" href="https://github.com/jgthms/bulma">
+          <i class="fa fa-github"></i>
+      </a>
+      </p>
+      </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+    <script src="https://use.fontawesome.com/eb044428f7.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <!-- Compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </body>
 </html>
--- header.php
+++ header.php
@@ -12,17 +12,71 @@ include 'database.php';
     <!-- responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
     <!-- bootstrap -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.min.css"> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.min.css">
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
+
+    <section class="hero is-info is-small">
+  <!-- Hero header: will stick at the top -->
+  <div class="hero-head">
+    <header class="nav">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item" href="/index.php">Contact Manager</a>
+        </div>
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div class="nav-right nav-menu">
+          <a class="nav-item is-active">
+            Home
+          </a>
+          <a class="nav-item" href="/index.php">All Contacts</a>
+          <a class="nav-item" href="/new.php">New Contacts</a>
+          <span class="nav-item">
+            <a class="button is-primary is-inverted">
+              <span class="icon">
+                <i class="fa fa-github"></i>
+              </span>
+              <span>Download</span>
+            </a>
+          </span>
+        </div>
+      </div>
+    </header>
+  </div>
+
+  <!-- Hero content: will be in the middle -->
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Contact Manager
+      </h1>
+
+      <h2 class="subtitle">
+        Useful tool to Edit and Creat your contacts
+      </h2>
+    </div>
+  </div>
+
+</section>
+
     <div class="container">
 
-      <nav class="navbar navbar-default">
+
+
+
+  <!-- <nav class="navbar navbar-default">
         <div class="container-fluid">
 
-          <!-- Brand and toggle get grouped for better mobile display -->
+
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span class="sr-only">Toggle navigation</span>
@@ -33,15 +87,19 @@ include 'database.php';
             <a class="navbar-brand" href="/index.php">Contact Manager</a>
           </div>
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
+
+
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
               <li><a href="/index.php">All Contacts</a></li>
               <li><a href="/new.php">New Contacts</a></li>
             </ul>
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-      </nav>
+          </div>
+
+
+        </div>
+      </nav> -->
+
 
       <div class="row">
 
